a400b105f7ca8af1deaa2571c89270ac70e19037
wonholim <kds0034@gmail.com>
2023-08-20 22:40:32 +0900

    fix : TBrowser run() 수정

--- network/TBrowser.ts
+++ network/TBrowser.ts
@@ -1,6 +1,19 @@
-/**
- *
- */
-export class TClient {
+import {TApplication} from "./TApplication";
+import {Message} from "./TNetworkInterface";
 
+/**
+ * Browser 클래스
+ * 서버에게 입력된 URL을 요청한다.
+ */
+export class TBrowser {
+    private tApplicattion: TApplication;
+    constructor() {
+        this.tApplicattion = new TApplication();
+    }
+
+    public run(url: string): string {
+        this.tApplicattion.encapsulation(url);
+
+        return "";
+    }
 }
